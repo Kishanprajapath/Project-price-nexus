6b171d0e5708d3ab872c2fca820fbebbd326530e
Kishanprajapath <prajapathkishan2004@gmail.com>
2023-11-01 00:48:18 +0530

    css

--- src/components/shopnav.jsx
+++ src/components/shopnav.jsx
@@ -1,36 +1,41 @@
 import React from "react";
 import "./shopnav.css";
 import logo from "./images/logo.png";
-import shoplogo from "./images/nameLogo.png";
+import namelogo from "./images/nameLogo.png";
 function Shopnav() {
   return (
-    <div className="shopmycontainer">
-      <div id="shopPNlogo">
-        <img id="ShopPNlogo" src={logo} alt="Logo"></img>
-        <img id="shopname-logo" src={shoplogo} alt="Name"></img>
-      </div>
-      <div className="shopnavalign">
-        <div className="categories">
-          <p id="shopdroptext">All</p>
-          <i id="shopdropdown" class="material-icons">keyboard_arrow_down</i>
+    <div className="shopfixeddiv>">
+      <div className="shopmycontainer">
+        <div id="shopPNlogo">
+          <img id="ShopPNlogo" src={logo} alt="Logo"></img>
+          <img id="shopname-logo" src={namelogo} alt="Name"></img>
         </div>
-        <div className="shopnavsearch">
-          <nav>
-            <div className="nav-wrapper">
-              <form>
-                <div className="input-field">
-                  <input
-                    id="search"
-                    className="search-inp"
-                    type="search"
-                    required></input>
-                  <i id="shopsearch-icon" className="material-icons">
-                    search
-                  </i>
-                </div>
-              </form>
-            </div>
-          </nav>
+        <div className="shopnavalign">
+          <div className="categories">
+            <p id="shopdropdowntext">All</p>
+            <i id="shopdropdown" class="material-icons">
+              keyboard_arrow_down
+            </i>
+          </div>
+          <div className="shopnavsearch">
+            <nav>
+              <div className="nav-wrapper">
+                <form>
+                  <div className="input-field">
+                    <input
+                      id="search"
+                      className="search-inp"
+                      type="search"
+                      placeholder="What are you looking for..."
+                      required></input>
+                    <i id="shopsearch-icon" className="material-icons">
+                      search
+                    </i>
+                  </div>
+                </form>
+              </div>
+            </nav>
+          </div>
         </div>
       </div>
     </div>
